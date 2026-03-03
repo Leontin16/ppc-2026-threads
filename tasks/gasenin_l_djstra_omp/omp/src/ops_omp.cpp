c8fefbe193e1bd758697073f95e7002a27fcda09
Leontin16 <gasenidze@mail.ru>
2026-03-03 18:04:26 +0300

    patch 1.21.2.1.0.0.6

--- tasks/gasenin_l_djstra_omp/omp/src/ops_omp.cpp
+++ tasks/gasenin_l_djstra_omp/omp/src/ops_omp.cpp
@@ -12,6 +12,99 @@
 
 namespace gasenin_l_djstra_omp {
 
+namespace {
+
+int GetNumThreads() {
+  int num_threads = 1;
+
+#pragma omp parallel default(none) shared(num_threads)
+  {
+#pragma omp single
+    num_threads = omp_get_num_threads();
+  }
+
+  return num_threads;
+}
+
+InType FindGlobalVertexOMP(const InType n, const InType inf, std::vector<InType> &dist, std::vector<char> &visited,
+                           const int num_threads) {
+  std::vector<InType> local_min(num_threads, inf);
+  std::vector<InType> local_vertex(num_threads, -1);
+
+  InType global_vertex = -1;
+
+#pragma omp parallel default(none) shared(n, inf, dist, visited, local_min, local_vertex, global_vertex, num_threads)
+  {
+    const int thread_id = omp_get_thread_num();
+
+    InType thread_min = inf;
+    InType thread_vertex = -1;
+
+#pragma omp for nowait
+    for (int index = 0; index < n; ++index) {
+      if (!visited[index] && dist[index] < thread_min) {
+        thread_min = dist[index];
+        thread_vertex = index;
+      }
+    }
+
+    local_min[thread_id] = thread_min;
+    local_vertex[thread_id] = thread_vertex;
+
+#pragma omp barrier
+
+#pragma omp single
+    {
+      InType global_min = inf;
+
+      for (int t = 0; t < num_threads; ++t) {
+        if (local_min[t] < global_min) {
+          global_min = local_min[t];
+          global_vertex = local_vertex[t];
+        }
+      }
+
+      if (global_vertex != -1 && global_min != inf) {
+        visited[global_vertex] = 1;
+      } else {
+        global_vertex = -1;
+      }
+    }
+  }
+
+  return global_vertex;
+}
+
+void RelaxEdgesOMP(const InType n, const InType inf, const InType global_vertex, std::vector<InType> &dist,
+                   std::vector<char> &visited) {
+#pragma omp parallel for default(none) shared(n, inf, global_vertex, dist, visited)
+  for (int vertex = 0; vertex < n; ++vertex) {
+    if (!visited[vertex] && vertex != global_vertex) {
+      const InType weight = std::abs(global_vertex - vertex);
+
+      if (dist[global_vertex] != inf) {
+        const InType new_dist = dist[global_vertex] + weight;
+        dist[vertex] = std::min(dist[vertex], new_dist);
+      }
+    }
+  }
+}
+
+int64_t CalculateTotalSumOMP(const InType n, const InType inf, std::vector<InType> &dist) {
+  int64_t total_sum = 0;
+
+#pragma omp parallel for reduction(+ : total_sum) default(none) shared(n, inf, dist)
+  for (int i = 0; i < n; ++i) {
+    if (dist[i] != inf) {
+      total_sum += dist[i];
+    }
+  }
+
+  return total_sum;
+}
+
+}  // namespace
+
 GaseninLDjstraOMP::GaseninLDjstraOMP(const InType &in) {
   SetTypeOfTask(GetStaticTypeOfTask());
   GetInput() = in;
@@ -37,88 +130,19 @@ bool GaseninLDjstraOMP::RunImpl() {
   const InType n = GetInput();
   const InType inf = std::numeric_limits<InType>::max();
 
-  auto &dist = dist_;
-  auto &visited = visited_;
+  const int num_threads = GetNumThreads();
 
-  int num_threads = 1;
+  for (int iteration = 0; iteration < n; ++iteration) {
+    InType global_vertex = FindGlobalVertexOMP(n, inf, dist_, visited_, num_threads);
 
-#pragma omp parallel default(none) shared(num_threads)
-  {
-#pragma omp single
-    num_threads = omp_get_num_threads();
-  }
-
-  std::vector<InType> local_min(num_threads, inf);
-  std::vector<InType> local_vertex(num_threads, -1);
-
-  InType global_vertex = -1;
-
-#pragma omp parallel default(none) shared(n, inf, dist, visited, local_min, local_vertex, global_vertex, num_threads)
-  {
-    const int thread_id = omp_get_thread_num();
-
-    for (int iteration = 0; iteration < n; ++iteration) {
-      InType thread_min = inf;
-      InType thread_vertex = -1;
-
-#pragma omp for nowait
-      for (int index = 0; index < n; ++index) {
-        if (visited[index] == 0 && dist[index] < thread_min) {
-          thread_min = dist[index];
-          thread_vertex = index;
-        }
-      }
-
-      local_min[thread_id] = thread_min;
-      local_vertex[thread_id] = thread_vertex;
-
-#pragma omp barrier
-
-#pragma omp single
-      {
-        global_vertex = -1;
-        InType global_min = inf;
-
-        for (int thread_idx = 0; thread_idx < num_threads; ++thread_idx) {
-          if (local_min[thread_idx] < global_min) {
-            global_min = local_min[thread_idx];
-            global_vertex = local_vertex[thread_idx];
-          }
-        }
-
-        if (global_vertex != -1 && global_min != inf) {
-          visited[global_vertex] = 1;
-        }
-      }
-
-#pragma omp barrier
-
-      if (global_vertex == -1) {
-        break;
-      }
-
-#pragma omp for
-      for (int vertex = 0; vertex < n; ++vertex) {
-        if (visited[vertex] == 0 && vertex != global_vertex) {
-          const InType weight = std::abs(global_vertex - vertex);
-
-          if (dist[global_vertex] != inf) {
-            const InType new_dist = dist[global_vertex] + weight;
-            dist[vertex] = std::min(dist[vertex], new_dist);
-          }
-        }
-      }
+    if (global_vertex == -1) {
+      break;
     }
+
+    RelaxEdgesOMP(n, inf, global_vertex, dist_, visited_);
   }
 
-  int64_t total_sum = 0;
-
-#pragma omp parallel for reduction(+ : total_sum) default(none) shared(n, dist, inf)
-  for (int index = 0; index < n; ++index) {
-    if (dist[index] != inf) {
-      total_sum += dist[index];
-    }
-  }
+  const int64_t total_sum = CalculateTotalSumOMP(n, inf, dist_);
 
   GetOutput() = static_cast<OutType>(total_sum);
   return true;
